82ac4cfbfb9e2042a00b49cbf2d543dc1a8ec0e7
Mathieu Robichaud <mathieu@standish.ca>
2020-05-15 10:20:51 -0400

    Added cors headers support

--- meal/settings.py
+++ meal/settings.py
@@ -7,6 +7,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'api',
     'recipe',
@@ -14,6 +15,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
