5594b02843febc5fa51d601095c51bd4d2cadfcf
Mathieu Robichaud <mathieu@standish.ca>
2020-05-15 09:49:59 -0400

    Basic meal api structure

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="api-index")
+    path('', views.index, name="api-index"),
+    path('meals/', views.mealList, name="api-meal-list"),
+    path('meals/<str:pk>/', views.mealDetail, name="api-meal-detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from recipe.models import Meal
 
+from .serializers import MealSerializer
+
+@api_view(['GET'])
 def index(request):
-    return JsonResponse({'message': 'abc'})
+    return Response({'message': 'abc'})
+
+
+@api_view(['GET', 'POST'])
+def mealList(request):
+    if request.method == 'POST':
+        data = request.data
+        #data['user'] = request.user.id
+        serialiser = MealSerializer(data=data)
+
+        if serialiser.is_valid():
+            serialiser.save()
+        else:
+            return Response(serialiser.errors)
+
+        return Response(serialiser.data)
+    else:
+        meals = Meal.objects.all() #filter(user=request.user)
+        serialiser = MealSerializer(meals, many=True)
+        return Response(serialiser.data)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def mealDetail(request, pk):
+    meal = Meal.objects.get(id=pk)
+
+    if request.method == 'DELETE':
+        meal.delete()
+        return Response({
+            "ok": "ok"
+        })
+    elif request.method == 'POST':
+        serializer = MealSerializer(instance=meal, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serialiser.errors)
+
+        return Response(serializer.data)
+    else:
+        serialiser = MealSerializer(meal, many=False)
+        return Response(serialiser.data)
