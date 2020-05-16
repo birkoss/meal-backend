1b621e22ec7d09ad67a7a3a5ffa0b443f27b8796
Mathieu Robichaud <mathieu@standish.ca>
2020-05-16 14:25:09 -0400

    Added foreign key into serialiser

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,28 @@
 from rest_framework import serializers
 
-
-from recipe.models import Meal, MealType
+from recipe.models import Meal, MealType, Recipe
 from user.models import User
-
-class MealSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meal
-        fields = '__all__'
 
 
 class MealTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = MealType
-        fields = '__all__'
+        fields = ['id', 'name']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id', 'name', 'url']
+
+
+class MealSerializer(serializers.ModelSerializer):
+    type = MealTypeSerializer(many=False, read_only=True)
+    recipe = RecipeSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Meal
+        fields = ['id', 'date_added', 'day', 'type', 'recipe']
 
 
 class UserSerializer(serializers.ModelSerializer):
